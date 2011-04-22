b0a4700282d9f1a5bb41349fd84f01e0c2d8efec
Minkyu Kim <synthdnb@gmail.com>
2011-04-23 04:55:26 +0900

    added default case for parsing wildcards

--- minishell.c
+++ minishell.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
+typedef struct command{
+	char *proc_name;
+	char **argv;
+} command;
+
 char * get_prompt(){
 	char *buf = (char *)malloc(sizeof(char)*1024);
 	printf("%s $ ",get_current_dir_name());
 	gets(buf);
 	return buf;
 }
+
+char ** parse_wildcards(char * cmd){
+	char ** ret;
+	if(strchr(cmd,'?')){
+	}else if(strchr(cmd,'*'){
+	}else{
+		ret = (char**)malloc(sizeof(char*));
+		strcpy(*ret,cmd);
+	}
+	return ret;
+}
 int main(){
 	char * cmd;
+	char ** cmdlist;
 	while(1){
 		cmd = get_prompt();
+		cmdlist = parse_wildcards(cmd);
 		free(cmd);
+
 	}
 	return 0;
 }
