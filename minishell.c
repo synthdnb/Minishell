ebee736eec54746dcaa02edaa0b572d931d96f60
Minkyu Kim <synthdnb@gmail.com>
2011-04-25 22:29:21 +0900

    Implemented Parse Syntax Error

--- minishell.c
+++ minishell.c
@@ -6,6 +6,7 @@
 #include <malloc.h>
 #include <fcntl.h>
 #include <wait.h>
+#include <stdlib.h>
 
 typedef struct command{
 	char *proc_name;
@@ -16,8 +17,10 @@ void trim(char *buf){
 	char *x;
 	int i,len;
 	x = buf;
-	while(*x == ' ' || *x == '\t' || *x == '\n')
+	while(*x == ' ' || *x == '\t' || *x == '\n'){
+		if(*x == 0){*buf = 0; return;}
 		x++;
+	}
 	len = strlen(x);
 	for(i=0;i<len;i++)
 		buf[i]=*(x+i);
@@ -213,6 +216,7 @@ proc_list *parse_proc(char *cmd){
 		strncpy(ptrcmd,str,p-str);
 		*(ptrcmd+(p-str)) = 0;
 		trim(ptrcmd);
+		if(strlen(ptrcmd) == 0) {ptr->redir_type =-1; ptr->next = NULL; free(ptrcmd); str = p+1; continue;}
 		if(ptrcmd[strlen(ptrcmd)-1] == '&'){
 			ptrcmd[strlen(ptrcmd)-1] = 0;
 			ptr->run_background = 1;
@@ -235,14 +239,17 @@ proc_list *parse_proc(char *cmd){
 			ed=st;
 			while(!(*ed == ' ' || *ed == '\t' || *ed == '>' || *ed == '<' || *ed == '|' || *ed == 0 || *ed == '\n'))
 				ed++;
-			ptr->redir = (char *)malloc(sizeof(char)*(ed-st+1));
-			strncpy(ptr->redir,st,ed-st);
-			*(ptr->redir+(ed-st)) = 0;
-			strcpy(pt,ed);
+			if(st==ed) ptr->redir_type = -1;
+			else{
+				ptr->redir = (char *)malloc(sizeof(char)*(ed-st+1));
+				strncpy(ptr->redir,st,ed-st);
+				*(ptr->redir+(ed-st)) = 0;
+				strcpy(pt,ed);
+			}
 		}else{
 			ptr->redir = NULL;
 		}
-		ptr->argv = str2argv(ptrcmd);
+		if(ptr->redir_type != -1) ptr->argv = str2argv(ptrcmd);
 		str = p+1;
 		ptr->next = NULL;
 		free(ptrcmd);
@@ -257,13 +264,14 @@ proc_list *parse_proc(char *cmd){
 	ptrcmd = (char *)malloc(sizeof(char)*(strlen(str)+1));
 	strcpy(ptrcmd,str);
 	trim(ptrcmd);
+	if(strlen(ptrcmd) == 0) { ptr->redir_type = -1; ptr->next = NULL; free(ptrcmd); return ret;}
 	if(ptrcmd[strlen(ptrcmd)-1] == '&'){
 		ptrcmd[strlen(ptrcmd)-1] = 0;
 		ptr->run_background = 1;
 	}else{
 		ptr->run_background = 0;
 	}
-	if((pt=strstr(ptrcmd,">>") != NULL)){
+	if((pt=strstr(ptrcmd,">>")) != NULL){
 		ptr->redir_type = 3;
 	}else if((pt=strchr(ptrcmd,'>')) != NULL){
 		ptr->redir_type = 2;
@@ -279,15 +287,17 @@ proc_list *parse_proc(char *cmd){
 		ed=st;
 		while(!(*ed == ' ' || *ed == '\t' || *ed == '>' || *ed == '<' || *ed == '|' || *ed == 0 || *ed == '\n'))
 			ed++;
-		ptr->redir = (char *)malloc(sizeof(char)*(ed-st+1));
-		strncpy(ptr->redir,st,ed-st);
-		*(ptr->redir+(ed-st)) = 0;
-		strcpy(pt,ed);
+		if(st == ed) ptr->redir_type = -1;
+		else{
+			ptr->redir = (char *)malloc(sizeof(char)*(ed-st+1));
+			strncpy(ptr->redir,st,ed-st);
+			*(ptr->redir+(ed-st)) = 0;
+			strcpy(pt,ed);
+		}
 	}else{
 		ptr->redir = NULL;
 	}
-
-	ptr->argv = str2argv(ptrcmd);
+	if(ptr->redir_type != -1) ptr->argv = str2argv(ptrcmd);
 	ptr->next = NULL;
 	free(ptrcmd);
 	return ret;
@@ -296,19 +306,27 @@ proc_list *parse_proc(char *cmd){
 void free_proc(proc_list *x){
 	if(!x) return;
 	int i;
-	for(i=0;x->argv[i] != NULL;i++)
-		free(x->argv[i]);
-	free(x->argv);
-	if(x->redir_type != 0)
-		free(x->redir);
-	if(x->next) free_proc(x->next);
+	if(x->redir_type != -1){
+		for(i=0;x->argv[i] != NULL;i++)
+			free(x->argv[i]);
+		free(x->argv);
+		if(x->redir_type != 0)
+			free(x->redir);
+		if(x->next) free_proc(x->next);
+	}
 	free(x);
 }
 
 void exec_proc(proc_list *proclist){
 	int fd[2];
 	int ifile,ofile,pid,status;
+	proc_list *chk;
 	ifile = STDIN_FILENO;
+	chk = proclist;
+	while(chk){
+		if(chk->redir_type <0){fprintf(stderr,"Parse Error\n"); return;}
+		chk = chk->next;
+	}
 	while(proclist){
 		if(proclist->next){
 			if(pipe(fd) < 0){
@@ -325,7 +343,7 @@ void exec_proc(proc_list *proclist){
 			}else if(proclist->redir_type == 2){
 				ofile = open(proclist->redir,O_WRONLY | O_CREAT, 0644);
 			}else if(proclist->redir_type == 3){
-				ofile = open(proclist->redir,O_APPEND | O_CREAT, 0644);
+				ofile = open(proclist->redir,O_WRONLY | O_APPEND | O_CREAT, 0644);
 			}
 		}
 		pid = fork();
@@ -333,11 +351,13 @@ void exec_proc(proc_list *proclist){
 			fprintf(stderr,"Fork Failed\n");
 			exit(1);
 		}else if(pid == 0){ //Child
+			signal (SIGINT,SIG_DFL);
+			signal (SIGTSTP,SIG_DFL);
 			if(ifile != STDIN_FILENO){
 				dup2(ifile,STDIN_FILENO);
 				close(ifile);
 			}
- 		if(ofile != STDOUT_FILENO){
+	 		if(ofile != STDOUT_FILENO){
 				dup2(ofile,STDOUT_FILENO);
 				close(ofile);
 			}
@@ -367,22 +387,26 @@ int main(){
 	char *cmd;
 	exec_list *execlist,*ptr;
 	int status;
+	signal (SIGTSTP,SIG_IGN);
+	signal (SIGINT,SIG_IGN);
 	while(1){
 		cmd = get_prompt();
 		if(strcmp(cmd,"exit") == 0){free(cmd); break;}
-		execlist = parse_wildcards(cmd);
-		free(cmd);
-		if(execlist == NULL) fprintf(stderr,"No Matching Wildcards\n");
-		else{
-			while(execlist != NULL){
-				execute(execlist->cmd);
-				free(execlist->cmd);
-				ptr = execlist->next;
-				free(execlist);
-				execlist = ptr;
+		if(strlen(cmd) != 0){
+			execlist = parse_wildcards(cmd);
+			free(cmd);
+			if(execlist == NULL) fprintf(stderr,"No Matching Wildcards\n");
+			else{
+				while(execlist != NULL){
+					execute(execlist->cmd);
+					free(execlist->cmd);
+					ptr = execlist->next;
+					free(execlist);
+					execlist = ptr;
+				}
 			}
+			waitpid(-1,&status,WNOHANG);
 		}
-		waitpid(-1,&status,WNOHANG);
 		
 	}
 	waitpid(-1,&status,WNOHANG);
